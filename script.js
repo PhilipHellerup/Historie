0938c06a30481470ebb4a0d8e594a44d81a6fb60
Alix2044 <Ali.jabarkhail@yahoo.com>
2022-11-02 13:32:48 +0100

    gg

---
+++ script.js
@@ -0,0 +1,24 @@
+let myQuestions = [
+	{
+		question: "Hvornår ud kom ...",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '115'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "Andet spørgsmål?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	}
+];
+class Quiz(q,a){
+
+    Quiz(i)
+}
