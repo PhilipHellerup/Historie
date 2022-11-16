ef7b2f4ee006a5b805fb1661d75266b3592ac2c2
Alix2044 <Ali.jabarkhail@yahoo.com>
2022-11-16 09:23:42 +0100

    h

--- Marvel.js
+++ Marvel.js
@@ -1,9 +1,33 @@
 
-let scoreTal = document.getElementById("tal")
-scoreTal.innerHTML ="0";
 
-let svar1 = document.getElementById("ms4").onclick = function(){
-    scoreTal.innerHTML ="10";
+let point = document.getElementById("tal")
+let score = 0;
+
+document.getElementById("svar1").onclick = function(){
+    score +=10;
+    point.innerHTML = score;
+    document.getElementById("svar1").disabled = true
 
 }
 
+document.getElementById("svar2").onclick = function(){
+    score +=10;
+    point.innerHTML = score;
+    document.getElementById("svar2").disabled = true
+
+}
+document.getElementById("svar3").onclick = function(){
+    score +=10;
+    point.innerHTML = score;
+    document.getElementById("svar3").disabled = true
+}
+document.getElementById("svar4").onclick = function(){
+    score +=10;
+    point.innerHTML = score;
+    document.getElementById("svar4").disabled = true
+}
+document.getElementById("svar5").onclick = function(){
+    score +=10;
+    point.innerHTML = score;
+    document.getElementById("svar5").disabled = true
+}
